b84a7e6133e649acc982a45188c405a51579e8ee
Dawn Ribeiro <wildorchid1006@yahoo.com>
2019-06-12 08:08:29 -0400

    wokroing

---
+++ week-7/AWholeNewWorld/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace AWholeNewWorld
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var min = 1;
+      var max = 100;
+      var Input = "";
+      Console.WriteLine("What is your favorite number between 1 and 100?");
+      while (Input != "yes")
+      {
+        var Guess = ((min + max) / 2);
+
+        Console.WriteLine($"Is your number {Guess}?");
+        Input = Console.ReadLine();
+
+        if (Input == "no")
+        {
+          Console.WriteLine("Is your number higher or lower?");
+          Input = Console.ReadLine();
+          if (Input == "higher")
+          {
+            min = Guess;
+          }
+          else if (Input == "lower")
+          {
+            max = Guess;
+          }
+        }
+      }
+
+      Console.WriteLine("Woohoo!!!");
+
+
+
+
+
+
+
+
+    }
+  }
+}
+
