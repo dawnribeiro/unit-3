879088371ccb96d77f20229930f943d0cac1d76c
Dawn Ribeiro <wildorchid1006@yahoo.com>
2019-06-17 08:15:04 -0400

    worked on sum

--- week-7/SafariVacation/Program.cs
+++ week-7/SafariVacation/Program.cs
@@ -7,8 +7,6 @@ namespace SafariVacation
   {
     static void Main(string[] args)
     {
-
-
       var db = new AnimalsContext();
       var input = "";
 
@@ -18,51 +16,40 @@ namespace SafariVacation
       // var allLions = db.AnimalsSeen.Where(animal => animal.Species == "lion");
       // db.AnimalsSeen.RemoveRange(allLions);
 
-
-
       // db.AnimalsSeen.Add(new AnimalsSeen
       // {
       //   Species = "lion",
       //   CountOfTimesSeen = 3,
       //   LocationOfLastSeen = "africa"
-
       // };
       // db.SaveChanges();
 
-
-
-      Console.WriteLine("Lets go on a Safari");
-      Console.WriteLine("What animals did you see, how many times, and where?");
+      Console.WriteLine("I heard you went on a Safari!");
+      Console.WriteLine("What animals did you see, how many times, and where did you see them or would you like to see a list of the animals you saw at a (location)");
       input = Console.ReadLine();
-      if (input == "")
+      if (input == "location")
       {
         db.SaveChanges();
-        Console.WriteLine("Wow! What was your favorite location?");
+        Console.WriteLine("What was your favorite location?");
         input = Console.ReadLine();
         var favePlace = db.AnimalsSeen.Where(seen => seen.LocationOfLastSeen == input).OrderBy(seen => seen.Species);
         foreach (var seen in favePlace)
-
         {
           Console.WriteLine($"You saw {favePlace} while you were there");
         }
-
-
       }
-
+      // var sum = db.AnimalsSeen.Sum(CountOfTimesSeen);
       else
       {
         var data = input.Split(", ");
         var newAnimal = new AnimalsSeen
-
         {
           Species = data[0],
           CountOfTimesSeen = int.Parse(data[1]),
           LocationOfLastSeen = data[2]
         };
-
         db.AnimalsSeen.Add(newAnimal);
         db.SaveChanges();
-
       }
     }
   }
